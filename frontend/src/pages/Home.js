9b586000ccf26b330c0164c5fcca6597ad78798d
Artur Gol <artur.golavskiy@metropolia.fi>
2025-02-06 18:49:39 +0200

    init project, added gameboy form

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GameBoy from "../components/GameBoy";
+
+const Home = () => {
+  return (
+    <div>
+      <GameBoy />
+    </div>
+  );
+};
+
+export default Home;
